f26cceb2fb3444c756eba781a808694437445088
Antonio Tsai <jimmyjack7777@gmail.com>
2016-03-01 17:01:42 +0800

    add remove Ad function

--- SFcomicViewer.user.js
+++ SFcomicViewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SFcomic Viewer
 // @namespace    
-// @version      0.1
+// @version      0.2.1
 // @description  Auto load comic picture.
 // @author       AntonioTsai
 // @match        http://comic.sfacg.com/*
@@ -10,19 +10,30 @@
 // @downloadURL  https://github.com/AntonioTsai/SFcomic-Viewer/raw/master/SFcomicViewer.user.js
 // ==/UserScript==
 
+var ADblocker = new function() {
+	for (var a = document.querySelector("iframe"); a; a = document.querySelector("iframe")) {
+		a.parentElement.removeChild(a);
+	}
+};
+
 var Viewer = new function() {
-  var imgtable = document.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0];
- 	var tempTr = document.createElement("tr");
- 	var imgs = document.createElement("img");
+	var imgtable = document.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0];
+	var tempTr = document.createElement("tr");
+	var imgs = document.createElement("img");
+	tempTr.appendChild(document.createElement("td"));
+	tempTr.children[0].appendChild(imgs);
 
- 	tempTr.appendChild(document.createElement("td"));
- 	tempTr.children[0].appendChild(imgs)
-
-  for (var i = 0; i < picCount; i++) {
-  	var imgTr = tempTr.cloneNode(true);
-  	imgTr.appendChild(document.createElement("img"));
-  	imgTr.children[0].children[0].src = picAy[i];
-  	imgtable.appendChild(imgTr);
-  }
+	// remove original image
+	var tr = document.querySelector("tr");
+	tr.parentElement.removeChild(tr);
+	
+	// generate all images
+	for (var i = 0; i < picCount; i++) {
+		var imgTr = tempTr.cloneNode(true);
+		imgTr.appendChild(document.createElement("img"));
+		imgTr.children[0].children[0].src = picAy[i];
+		imgtable.appendChild(imgTr);
+	}
+	
 
 };
