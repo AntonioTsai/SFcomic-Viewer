a1da554a2321f3ca6f93687c8eb3af00db60b49f
Antonio Tsai <antonio.tsaii@gmail.com>
2018-09-29 14:40:28 +0800

    Declare global variable for standardjs

--- SFcomicViewer.user.js
+++ SFcomicViewer.user.js
@@ -11,11 +11,7 @@
 // @downloadURL  https://github.com/AntonioTsai/SFcomic-Viewer/raw/master/SFcomicViewer.user.js
 // ==/UserScript==
 
-/**
- * picAy: The array store all image's url of the volume
- * hosts: The array store possible host
- * getHost(): Get the current host index
- */
+/* global picAy */
 (() => {
   const imgTable = document.querySelector('table tbody')
   const tempTr = document.createElement('tr')
@@ -25,6 +21,7 @@
   imgTable.querySelector('tr').remove()
 
   // Render all images
+  // picAy - global array defined by site
   picAy.map((pic) => {
     const imgTr = tempTr.cloneNode(true)
     const img = document.createElement('img')
