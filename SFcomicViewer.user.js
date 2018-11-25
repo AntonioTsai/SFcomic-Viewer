f8720f3bfc70f8477a3994a73207850a608322fa
Antonio Tsai <antonio.tsaii@gmail.com>
2018-11-26 00:40:14 +0800

    Fix error occurs sometimes

--- SFcomicViewer.user.js
+++ SFcomicViewer.user.js
@@ -17,54 +17,56 @@
  * {Array} hosts - Array of possible host
  * {Function} getHost - Return the index of selected host in string type
  */
-(() => {
-  const imgTable = document.querySelector('table tbody')
-  const tempTr = document.createElement('tr')
-  tempTr.appendChild(document.createElement('td'))
+window.onload = _ => {
+  (_ => {
+    const imgTable = document.querySelector('table tbody')
+    const tempTr = document.createElement('tr')
+    tempTr.appendChild(document.createElement('td'))
 
-  let host = ''
-  if (Array.isArray(hosts) & typeof getHost === 'function') {
-    host = hosts[parseInt(getHost())]
-  }
+    let host = ''
+    if (typeof hosts !== 'undefined' && Array.isArray(hosts) && typeof getHost === 'function') {
+      host = hosts[parseInt(getHost())]
+    }
 
-  // Remove original image & social media icon
-  imgTable.querySelector('tr').remove()
+    // Remove original image & social media icon
+    imgTable.querySelector('tr').remove()
 
-  // Render all images
-  // picAy - global array defined by site
-  picAy.map((pic) => {
-    const imgTr = tempTr.cloneNode(true)
-    const img = document.createElement('img')
+    // Render all images
+    // picAy - global array defined by site
+    picAy.map((pic) => {
+      const imgTr = tempTr.cloneNode(true)
+      const img = document.createElement('img')
 
-    img.src = host + pic
-    img.className = 'scalable'
-    imgTr.querySelector('td').appendChild(img)
-    imgTable.appendChild(imgTr)
-  })
+      img.src = host + pic
+      img.className = 'scalable'
+      imgTr.querySelector('td').appendChild(img)
+      imgTable.appendChild(imgTr)
+    })
 
-  // Add CSS style
-  let link = document.createElement('link')
-  const cssFileUrl = 'https://raw.githubusercontent.com/AntonioTsai/SFcomic-Viewer/master/css/style.css'
-  link.setAttribute('rel', 'stylesheet')
-  link.setAttribute('type', 'text/css')
-  link.setAttribute('href', cssFileUrl)
-  document.head.appendChild(link)
+    // Add CSS style
+    let link = document.createElement('link')
+    const cssFileUrl = 'https://antoniotsai.github.io/SFcomic-Viewer/css/style.css'
+    link.setAttribute('rel', 'stylesheet')
+    link.setAttribute('type', 'text/css')
+    link.setAttribute('href', cssFileUrl)
+    document.head.appendChild(link)
 
-  const topPagination = document.querySelector('.Reduction_top + .wrap > .page_turning')
+    const topPagination = document.querySelector('.Reduction_top + .wrap > .page_turning')
 
-  // Remove tips
-  topPagination.lastChild.remove()
+    // Remove tips
+    topPagination.lastChild.remove()
 
-  // Remove unnecessary NextPage & PrePage button
-  // Select the button on the top
-  const topButtons = topPagination.querySelectorAll(':not(:nth-child(n+6)):not(:nth-child(-n+2))')
-  topButtons.forEach((current) => {
-    current.remove()
-  })
+    // Remove unnecessary NextPage & PrePage button
+    // Select the button on the top
+    const topButtons = topPagination.querySelectorAll(':not(:nth-child(n+6)):not(:nth-child(-n+2))')
+    topButtons.forEach((current) => {
+      current.remove()
+    })
 
-  // Select the button at the bottom
-  const bottomButtons = document.querySelectorAll('table + .wrap > .page_turning > :not(:nth-child(n+5)):not(:nth-child(-n+1))')
-  bottomButtons.forEach((current) => {
-    current.remove()
-  })
-})()
+    // Select the button at the bottom
+    const bottomButtons = document.querySelectorAll('table + .wrap > .page_turning > :not(:nth-child(n+5)):not(:nth-child(-n+1))')
+    bottomButtons.forEach((current) => {
+      current.remove()
+    })
+  })()
+}
